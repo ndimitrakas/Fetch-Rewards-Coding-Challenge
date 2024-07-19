e96d9b272012c703a1ba7abe59d6593efd30c6b3
Nickolas Dimitrakas <nickgr123@gmail.com>
2024-07-19 17:48:04 -0400

    Inital code given by xcode for app

---
+++ FetchCodingChallenge/FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Nick Dimitrakas on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
