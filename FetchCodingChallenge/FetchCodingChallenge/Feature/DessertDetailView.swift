37d9384cef2044779d8b59ed503554d03e16a225
Nickolas Dimitrakas <nickgr123@gmail.com>
2024-07-19 17:56:36 -0400

    Added detail view to the app

---
+++ FetchCodingChallenge/FetchCodingChallenge/Feature/DessertDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertDetailView.swift
+//  FetchCodingChallenge
+//
+//  Created by Nick Dimitrakas on 7/19/24.
+//
+
+import SwiftUI
+
+struct DessertDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DessertDetailView()
+}
